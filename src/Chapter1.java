cc68c326ce7f8b97e527e39bfa4c84f11075a0d2
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-02-18 13:32:56 -0500

    Chapter 1, Exercise 40

--- src/Chapter1.java
+++ src/Chapter1.java
@@ -3,9 +3,20 @@ import javax.swing.JOptionPane;
 public class Chapter1 {
 	public static void main(String[] args) {
 		/*
+		Chapter 1, Self-Test Question #39
 		Write a complete Java program that produces a dialog window containing the message HelloWorld!.
 		Your program does nothing else.
-	 */
+	    */
 		JOptionPane.showMessageDialog(null, "HelloWorld");
+
+		/*
+		Chapter 1, Self-Test Question #40
+		Write a complete Java program that behaves as follows. The program displays an input dialog window
+		asking the user to enter a whole number. When the user enters a whole number and clicks the OK button,
+		the window goes away, and an output dialog window appears. This window simply tells the user what number was entered.
+		When the user clicks the OK button in the window, the program ends.
+		 */
+		String input = JOptionPane.showInputDialog("Enter a whole number: ");
+		JOptionPane.showMessageDialog(null, input);
 	}
 }
