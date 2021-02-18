75ad1b1523b731fb53fbb2139a5224a4bcae75d0
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-02-18 13:28:32 -0500

    Selt-Test Question #39 Chapter 1

--- src/Chapter1.java
+++ src/Chapter1.java
@@ -1,3 +1,11 @@
-public class Chapter1 {
+import javax.swing.JOptionPane;
 
+public class Chapter1 {
+	public static void main(String[] args) {
+		/*
+		Write a complete Java program that produces a dialog window containing the message HelloWorld!.
+		Your program does nothing else.
+	 */
+		JOptionPane.showMessageDialog(null, "HelloWorld");
+	}
 }
