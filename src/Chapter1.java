e4704d54bd139297dcd92cb11039fd800da5509e
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-03-01 00:00:36 -0500

    BMR calculator

--- src/Chapter1.java
+++ src/Chapter1.java
@@ -103,122 +103,23 @@ public class Chapter1 {
 		// Practice Projects
 
 		/*
-			1. Write a program that converts degrees from Fahrenheit to Celsius, using the formula
-			DegreesC = 5(DegreesF - 32) / 9
+			8. The Harris-Benedict equation estimates the number of calories your body needs to maintain your weight if
+			you do no exercise. This is called your basal metabolic rate, or BMR. A typical chocolate bar will contain
+			around 230 calories. Write a program that allows the user to input his or her weight in pounds, height in
+			inches, and age in years. The program should then output the number of chocolate bars that should be consumed
+			to maintain one’s weight for both a woman and a man of the input weight, height, and age.
 		 */
-		System.out.print("Enter a temperature in degrees Fahrenheit: ");
-		int fahrenheit = keyboard.nextInt();
-		float celsius = 5 * (fahrenheit - 32) / 9f;
-		System.out.printf("%d degrees Fahrenheit is %.1f degrees Celsius.\n\n", fahrenheit, celsius);
+		System.out.print("Enter your weight (lbs): ");
+		int weight = keyboard.nextInt();
+		System.out.print("Enter your height (inches): ");
+		int height = keyboard.nextInt();
+		System.out.print("Enter your age (years): ");
+		int age = keyboard.nextInt();
 
-		/*
-			2. Write a program that reads a line of text and then displays the line, but with the first occurrence of
-			"hate" changed to love. For example, a possible sample dialogue might be
-			Enter a line of text.
-			I hate you.
-			I have rephrased that line to read:
-			I love you.
-		 */
-		// Code below is required to consume the new-line before cursor can be moved down to the needed line.
-		keyboard.nextLine();
-		System.out.println("Enter a line of text.");
-		String text = keyboard.nextLine();
-		System.out.println("I have rephrased that line to read:\n" + text.replaceFirst("hate", "love"));
+		double femaleBMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+		double maleBMR = 66 + (6.3 * weight) + (12.9 * height) - (6.8 * age);
 
-		/*
-			3. Write a program that will read a line of text as input and then display the line with the first word
-			moved to the end of the line. For example, a possible sample interaction with the user might be
-
-			Enter a line of text. No punctuation please.
-			Java is the language
-			I have rephrased that line to read
-			Is the language Java
-
-		 */
-		System.out.println("Enter a line of text. No punctuation please.");
-		String sentence = keyboard.nextLine();
-		String firstWord = sentence.substring(0, sentence.indexOf(" "));
-		sentence = sentence.replaceFirst(firstWord, "") + " " + firstWord;
-		System.out.println("I have rephrased that line to read:\n" + sentence);
-
-		/*
-			4. Write a program that asks the user to enter a favorite color, a favorite food, a favorite animal, and the
-			first name of a friend or relative. The program should then print the following two lines, with the user’s
-			input replacing the items in quotes:
-
-			I had a dream that "Name" ate a Color "Animal" and said it tasted like "Food"!
-		 */
-		System.out.print("Enter a name: ");
-		String name = keyboard.next();
-		System.out.print("Enter a color: ");
-		String color = keyboard.next();
-		System.out.print("Enter an animal: ");
-		String animal = keyboard.next();
-		System.out.print("Enter a food: ");
-		String food = keyboard.next();
-
-		System.out.printf("I had a dream that %s ate a %s %s and said it tasted like %s!\n", name, color, animal, food);
-
-		/*
-			5. Write a program that determines the change to be dispensed from a vending machine. An item in the can cost
-			between 25 cents and a dollar, in 5-cent increments (25, 30, 35, ..., 90, 95, 100), and the machine accepts
-			only a single dollar bill to pay for the item. For example, a possible dialogue with the use might be
-			Enter price of item (from 25 cents to a dollar, in 5-cent increments): 45
-
-			You bought an iterm for 45 cents and gave me a dollar, so your change is
-			2 quarters,
-			0 dimes, and
-			1 nickel.
-		 */
-		System.out.print("Enter price of item (from 25 cents to a dollar, in 5-cent increments): ");
-		int price = keyboard.nextInt();
-		int change = 100 - price;
-
-		int quarter =  change / 25;
-		change %= 25;
-
-		int dime = change / 10;
-		change %= 10;
-
-		int nickel = change / 5;
-
-		System.out.printf("You bought an item for %d cents and gave me a dollar, so your change is" +
-						  "\n%d quarters\n%d dimes, and \n%d nickel.\n", price, quarter, dime, nickel);
-
-		/*
-			6. Write a program that reads a 4-bit binary number from the keyboard as a string and then converts it into
-			decimal. For example, if the input is 1100, the output should be 12.
-		 */
-		System.out.print("Enter 4-bit binary number: ");
-		String binary = keyboard.next();
-		int one = Integer.parseInt(binary.substring(0, 1));
-		int two = Integer.parseInt(binary.substring(1, 2));
-		int three = Integer.parseInt(binary.substring(2, 3));
-		int four = Integer.parseInt(binary.substring(3, 4));
-
-		int decimal = 8 * one + 4 * two + 2 * three + four;
-		System.out.println(binary + " in binary is " + decimal + " in decimal.");
-
-		/*
-			7. Many private water wells produce only 1 or 2 gallons of water per minute. One way to avoid running out of
-			water with these low-yield wells is to use a holding tank. A family of 4 will use about 250 gallons of water
-			per day. However, there is a “natural” water holding tank in the casing (i.e., the hole) of the well itself.
-			The  deeper  the  well,  the  more  water  that  will  be  stored that can be pumped out for household use.
-			But how much water will be available?
-
-			Write a program that allows the user to input the radius of the well casing in inches (a typical well will
-			have a 3-inch radius) and the depth of the  well  in  feet  (assume  water  will  fill  this  entire  depth,
-			although  in  practice that will not be true since the static water level will generally be 50 feet or more
-			below the ground surface). The program should output the number of gallons stored in the well casing. For
-			your reference:
-			The volume of a cylinder is πr2h , where r is the radius and h is the height. 1 cubic foot = 7.48 gallons of water.
-		 */
-		System.out.print("Enter the depth of well [in feet]: ");
-		double depth = keyboard.nextDouble();
-		System.out.print("Enter the radius [in inches]: ");
-		double radius = keyboard.nextDouble();
-		double inchToFeet = radius / 12;
-		double gallons = Math.PI * (inchToFeet * inchToFeet) * depth * 7.48;
-		System.out.println("The well can hold up to " + gallons + " gallons of water.");
+		System.out.println("To maintain your BMR you will need to eat\n" + (int) (femaleBMR / 230) + " chocolate bars if" +
+				" you are a female\nor\n" + (int) (maleBMR / 230) + " chocolate bars if you are a male!");
 	}
 }
