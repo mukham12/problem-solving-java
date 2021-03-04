2f9cc9d8510f9fc373f0e2f85ab7f27a671eeb0b
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-03-04 00:00:17 -0500

    Chapter 2, Exercise 28

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -1,2 +1,18 @@
+import javax.swing.JOptionPane;
+
 public class Chapter2 {
+	public static void main(String[] args) {
+		/*
+			Self-Test Question
+			23. Write code for a JOptionPane dialog that will ask whether the user is at least 18 years of age. Then set
+			the boolean variable 'adult' equal to either true, if the user is at least 18, or false otherwise. Include
+			the declaration of the variable 'adult'
+		 */
+		int age = JOptionPane.showConfirmDialog(null, "Are you 18 years old?", "Ask for age", JOptionPane.YES_NO_OPTION);
+		boolean adult = false;
+		if (age == JOptionPane.YES_OPTION) {
+			adult = true;
+			System.out.println("You are an adult!");
+		}
+	}
 }
