6b8b0d5ac807c8fb13d4cdf91e6b3242e1ddf2c0
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-03-04 00:04:14 -0500

    Chapter 2, practice program 1:

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -1,4 +1,5 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class Chapter2 {
 	public static void main(String[] args) {
@@ -13,6 +14,20 @@ public class Chapter2 {
 		if (age == JOptionPane.YES_OPTION) {
 			adult = true;
 			System.out.println("You are an adult!");
+		}
+
+		/*
+			Practice Programs
+			1. If a number x is multiplied by the number 0, the result is zero. Write a program that tests whether the
+			result is zero if one number is multiplied by 0. Read the number from the keyboard.
+		 */
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int x = keyboard.nextInt();
+		if (x * 0 == 0) {
+			System.out.println("Multiplication of " + x + " by 0 resulted in 0.");
+		} else {
+			System.out.println("This is impossible!");
 		}
 	}
 }
