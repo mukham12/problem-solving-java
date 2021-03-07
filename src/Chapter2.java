954f04f55561601eb981f50a696ebf9b321de033
mukham12 <Muhammad.Khalikov29@student.qcc.cuny.edu>
2021-03-07 13:13:47 -0500

    Sentence question/exclamation response

--- src/Chapter2.java
+++ src/Chapter2.java
@@ -82,5 +82,26 @@ public class Chapter2 {
 		} else {
 			System.out.print(s3 + "'");
 		}
+
+		/*
+			4. Write a program that reads a one-line sentence as input and then displays the following response: If the
+			sentence ends with a question mark (?) and the input contains an even number of characters, display the word
+			Yes. If the sentence ends with a question mark and the input contains an odd number of characters, display
+			the word No. If the sentence ends with an exclamation point (!), display the word Wow. In all other cases,
+			display the words You always say followed  by  the  input  string  enclosed  in  quotes.
+		 */
+		System.out.println("Enter a sentence: ");
+		String sentence = keyboard.nextLine();
+		if (sentence.charAt(sentence.length() - 1) == '?') {
+			if (sentence.length() % 2 == 0) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+		} else if (sentence.charAt(sentence.length() - 1) == '!'){
+			System.out.println("Wow");
+		} else {
+			System.out.println("You always say \"" + sentence + "\"");
+		}
 	}
 }
